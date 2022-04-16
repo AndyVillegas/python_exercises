948f1f30c15b7cc9a5007df251a5f3a00d7f7eb8
Andy Villegas <andyvillegas440@gmail.com>
2022-04-16 01:58:41 -0500

    remove unnecessary variables

--- hackerrank/python_string_formatting.py
+++ hackerrank/python_string_formatting.py
@@ -1,15 +1,11 @@
 def print_formatted(number):
     max_binary_length = len(bin(number)[2:])
     for n in range(1, number + 1):
-        decimal = str(n)
-        octal = oct(n)[2:]
-        hexadecimal = hex(n)[2:].upper()
-        binary = bin(n)[2:]
         print("{} {} {} {}".format(
-            decimal.rjust(max_binary_length),
-            octal.rjust(max_binary_length),
-            hexadecimal.rjust(max_binary_length),
-            binary.rjust(max_binary_length)))
+            str(n).rjust(max_binary_length),
+            oct(n)[2:].rjust(max_binary_length),
+            hex(n)[2:].upper().rjust(max_binary_length),
+            bin(n)[2:].rjust(max_binary_length)))
 
 if __name__ == '__main__':
     n = int(input())
